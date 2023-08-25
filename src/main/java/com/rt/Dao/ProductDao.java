929b286758adcc0fb5acab565e0951d3c6d21cce
Bhagyashri <bhagyashris500@gmail.com>
2023-08-25 15:55:43 +0530

    Your commit message here

--- src/main/java/com/rt/Dao/ProductDao.java
+++ src/main/java/com/rt/Dao/ProductDao.java
@@ -1,9 +1,17 @@
 package com.rt.Dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.rt.Entity.ProductEntity;
 
 public interface ProductDao extends JpaRepository<ProductEntity, Integer> {
+   @Query(value="select * from productdeatails where productName=? ", nativeQuery=true)
+	List<ProductEntity> findByName(String productName);
+
+	
+
 
 }
--- src/main/java/com/rt/ProductController/ProductController.java
+++ src/main/java/com/rt/ProductController/ProductController.java
@@ -1,6 +1,11 @@
 package com.rt.ProductController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +15,35 @@ import com.rt.Services.ProductService;
 
 @RestController
 public class ProductController {
-@Autowired
-ProductService pService;
+	@Autowired
+	ProductService pService;
 
-@PostMapping("/addProduct")
-	public String Product(@RequestBody ProductEntity p){
+	@PostMapping("/addProduct")
+	public String Product(@RequestBody ProductEntity p) {
 		pService.addProduct(p);
-		return"Product Add";
+		return "Product Add";
 	}
-	
+
+	@GetMapping("/addProduct/productId/{productId}")
+	public ProductEntity GetProductById(@PathVariable int productId) {
+		ProductEntity p = pService.GetProductById(productId);
+		return p;
+	}
+
+	@DeleteMapping("/addProduct/productId/{productId}")
+	public ProductEntity deleteProductById(@PathVariable int productId) {
+		ProductEntity p = pService.deleteProductById(productId);
+		return p;
+	}
+   @GetMapping("/product")
+   public List<ProductEntity> getAllEmp(){
+	 List<ProductEntity>   productList= pService.GetAllProduct();
+	return productList;
+   }
+   @GetMapping("/product/productName/{productName}")
+   public List<ProductEntity> getEmpName(@PathVariable String productName){
+	 List<ProductEntity>   productList= pService.getEmpName(productName);
+	return productList;
+   }
+
 }
